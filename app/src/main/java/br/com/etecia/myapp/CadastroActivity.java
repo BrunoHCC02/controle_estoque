4abf3af9b3c2a5f8c6779a9d9680c1f74017c16c
BrunoHCC02 <brhcc02@gmail.com>
2024-11-14 22:04:06 -0300

    login FUNCIONANDO

--- app/src/main/java/br/com/etecia/myapp/CadastroActivity.java
+++ app/src/main/java/br/com/etecia/myapp/CadastroActivity.java
@@ -1,6 +1,9 @@
 package br.com.etecia.myapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
 public class CadastroActivity extends AppCompatActivity {
+    MaterialToolbar idCadRetorno;
+    TextInputEditText idlogin,idsenha;
+    MaterialButton btnCadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +30,43 @@ public class CadastroActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        idCadRetorno = findViewById(R.id.idCadRetorno);
+
+        idCadRetorno.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+                finish();
+            }
+        });
+        btnCadastrar = findViewById(R.id.btnCadastrar);
+        idlogin = findViewById(R.id.textemail);
+        idsenha = findViewById(R.id.textsenha);
+
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String idemail = idlogin.getText().toString().trim();
+                String idpassword = idsenha.getText().toString().trim();
+
+                if (idemail.isEmpty() || idpassword.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Por favor, preencha todos os campos", Toast.LENGTH_SHORT).show();
+                } else {
+
+                    Toast.makeText(getApplicationContext(), "Cadastro realizado com sucesso!", Toast.LENGTH_SHORT).show();
+
+                    startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                    Intent intent = new Intent(CadastroActivity.this, LoginActivity.class);
+                    intent.putExtra("EMAIL", idemail);
+                    intent.putExtra("SENHA", idpassword);
+                    startActivity(intent);
+                    finish();
+
+
+                }
+            }
         });
     }
 }
--- app/src/main/java/br/com/etecia/myapp/LoginActivity.java
+++ app/src/main/java/br/com/etecia/myapp/LoginActivity.java
@@ -1,6 +1,9 @@
 package br.com.etecia.myapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+
 public class LoginActivity extends AppCompatActivity {
+
+    MaterialButton btnCadastrarSenha, btnEntrar;
+    TextInputEditText txtSenha, txtEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,41 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Intent intent = getIntent();
+        String idemail = intent.getStringExtra("EMAIL");  // Recuperando o e-mail
+        String idpassword = intent.getStringExtra("SENHA");  // Recuperando a senha
+
+        btnCadastrarSenha = findViewById(R.id.btnCadastrarSenha);
+        btnEntrar = findViewById(R.id.btnEntrar);
+        txtEmail = findViewById(R.id.txtEmail);
+        txtSenha = findViewById(R.id.txtSenha);
+
+        //criando as ações dos botões
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (txtEmail.getText().toString().equals(idemail) && txtSenha.getText().toString().equals(idpassword)) {
+                    startActivity(new Intent(getApplicationContext(),
+                            MenuprincipalActivity.class));
+                    finish();
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "Usuário ou senha inválidos",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+        btnCadastrarSenha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), CadastroActivity.class));
+                finish();
+            }
+        });
+
     }
 }
